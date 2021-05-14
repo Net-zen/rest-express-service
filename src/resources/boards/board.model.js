72e8c81ba49d3f6da33672367c74a7f3de2ac833
Net-zen <prankster@myttk.ru>
2021-05-14 14:07:28 +0600

    feat: add .eslintignore, add board model

---
+++ src/resources/boards/board.model.js
@@ -0,0 +1,22 @@
+const { v4: uuid } = require('uuid');
+
+
+class Board {
+  constructor({
+    id = uuid(),
+    title = 'Autotest board',
+    columns = [
+      {
+        id: uuid(),
+        title: 'Backlog',
+        order: 0
+      }
+    ]
+  } = {}) {
+    this.id = id;
+    this.title = title;
+    this.columns = columns;
+  }
+}
+
+module.exports = Board;
