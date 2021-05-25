ba24991f76bc93999ba68ad2c75bb2e9359bec1d
Net-zen <prankster@myttk.ru>
2021-05-25 23:28:41 +0600

    feat:  add jsdoc comments to user.memory.repository

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,7 +1,20 @@
+/**
+ * Module user.memory.repository
+ * @module userMemoryRepository
+ */
 const { getAllUsers, getUser, createUser, updateUser, removeUser } = require('../../common/inMemoryDb');
 
+/**
+ * Get all users from db
+ * @returns {Promise.<Object[]|array>} - all users or empty array
+ */
 const getAll = () => getAllUsers();
 
+/**
+ * Get user by id from db and check if user exist
+ * @param {string} id user id
+ * @returns {Promise.<Object|false>} - user or false
+ */
 const get = async id => {
   const user = await getUser(id);
   if (!user) {
@@ -10,10 +23,26 @@ const get = async id => {
   return user;
 };
 
+/**
+ * Add new user to db
+ * @param {string} user - user id
+ * @returns {Promise<Object|false>} - added user or false
+ */
 const create = user => createUser(user);
 
+/**
+ * Updates user in db
+ * @param {string} id - updatable user id
+ * @param {Object} user - new user data for update
+ * @returns {Promise<Object|false>} updated user or false
+ */
 const update = (id, user) => updateUser(id, user);
 
+/**
+ * Removes user from db
+ * @param {string} id - removable use id
+ * @returns {Promise<boolean>} - true if user removed or false
+ */
 const remove =  id => removeUser(id);
 
 module.exports = { getAll, get, create, update, remove };
