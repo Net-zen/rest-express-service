c556875f4dfba9ac64a848893d79c058fac3c293
Net-zen <prankster@myttk.ru>
2021-05-25 23:44:26 +0600

    feat:  add jsdoc comments to task.memory.repository

--- src/resources/tasks/task.memory.repository.js
+++ src/resources/tasks/task.memory.repository.js
@@ -1,5 +1,14 @@
+/**
+ * Module task.memory.repository
+ * @module taskMemoryRepository
+ */
 const { getAllTasksByBoard, getTask, createTask, updateTaskInBoard, removeTask } = require('../../common/inMemoryDb');
 
+/**
+ * Get all tasks by board id from db
+ * @param {string} boardId - board id where tasks bound to
+ * @returns {Promise<boolean|Object[]>} - return all tasks bound to board or false
+ */
 const getAll = async boardId => {
   const tasks = await getAllTasksByBoard(boardId);
 
@@ -9,6 +18,11 @@ const getAll = async boardId => {
   return tasks;
 };
 
+/**
+ * Get task by id from db and check if task exist
+ * @param {string} id task id
+ * @returns {Promise.<Object|false>} - task or false
+ */
 const get = async id => {
   const task = await getTask(id);
   if (!task) {
@@ -17,8 +31,20 @@ const get = async id => {
   return task;
 };
 
+/**
+ * Add new task to db
+ * @param {Object} task - task id
+ * @returns {Promise<Object|false>} - added task or false
+ */
 const create = task => createTask(task);
 
+/**
+ * Updates task in db
+ * @param {string} boardId -  board id where tasks bound to
+ * @param {string} id - updatable task id
+ * @param {Object} task - new task data for update
+ * @returns {Promise<Object|false>} updated task or false
+ */
 const update = async (boardId, id, task) => {
   const updatedTask = await updateTaskInBoard(boardId, id, task);
   if (!updatedTask) {
@@ -27,6 +53,12 @@ const update = async (boardId, id, task) => {
   return updatedTask;
 };
 
+/**
+ * Removes task from db
+ * @param {string} boardId - board id where tasks bound to
+ * @param {string} id - removable use id
+ * @returns {Promise<boolean>} - true if task removed or false
+ */
 const remove = async (boardId, id) => {
   const task = await removeTask(boardId, id);
   if (!task) {
