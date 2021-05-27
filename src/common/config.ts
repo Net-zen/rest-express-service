64275d44c9cd2b0cd81480d44704644ae56dc7ee
Net-zen <prankster@myttk.ru>
2021-05-27 12:44:29 +0600

    feat: start moving to typescript. install dependencies, setup configs

---
+++ src/common/config.ts
@@ -0,0 +1,17 @@
+import * as dotenv from 'dotenv';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+export const __dirname = dirname(fileURLToPath(import.meta.url));
+
+dotenv.config({
+  path: path.join(__dirname, '../../.env')
+});
+
+export const config = {
+  PORT: process.env['PORT'],
+  NODE_ENV: process.env['NODE_ENV'],
+  MONGO_CONNECTION_STRING: process.env['MONGO_CONNECTION_STRING'],
+  JWT_SECRET_KEY: process.env['JWT_SECRET_KEY'],
+  AUTH_MODE: process.env['AUTH_MODE'] === 'true'
+};
