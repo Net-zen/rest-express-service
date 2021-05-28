e505a14f69240ac38f916ce6b6610123c6a005d7
Net-zen <prankster@myttk.ru>
2021-05-28 14:10:42 +0600

    feat: add cors and helmet

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import swaggerUI from 'swagger-ui-express';
 import path from 'path';
 import YAML from 'yamljs';
+import helmet from 'helmet';
+import cors from 'cors';
 import userRouter from './resources/users/user.router';
 import boardRouter from './resources/boards/board.router';
 import taskRouter from './resources/tasks/task.router';
@@ -9,6 +11,9 @@ import taskRouter from './resources/tasks/task.router';
 const app = express();
 
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
+
+app.use(helmet());
+app.use(cors());
 
 app.use(express.json());
 
