ca0f6b4e2eb4bc5acc239b75858312c8eb6f9f96
Net-zen <prankster@myttk.ru>
2021-06-01 19:19:19 +0600

    feat: add logging, using winston and morgan

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import path from 'path';
 import YAML from 'yamljs';
 import helmet from 'helmet';
 import cors from 'cors';
+import morgan from 'morgan';
+import { logger } from './logging';
 import userRouter from './resources/users/user.router';
 import boardRouter from './resources/boards/board.router';
 import taskRouter from './resources/tasks/task.router';
@@ -16,6 +18,15 @@ app.use(helmet());
 app.use(cors());
 
 app.use(express.json());
+
+app.use(
+  morgan(
+    ':protocol :http-version :ip :method :status :url ' +
+    'query: :query ' +
+    'body: :body size :user-agent :res[content-length] - :response-time ms',
+    { stream: logger }
+  )
+);
 
 app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
@@ -32,5 +43,6 @@ app.use('/users', userRouter);
 app.use('/boards', boardRouter);
 
 boardRouter.use('/:boardId/tasks', taskRouter);
+
 
 export default app;
---
+++ src/logging/index.ts
@@ -0,0 +1,92 @@
+import winston from 'winston';
+import 'winston-daily-rotate-file';
+import morgan, { StreamOptions } from 'morgan';
+import { IncomingMessage } from 'http';
+import { NODE_ENV } from '../common/config';
+
+interface IRequest extends IncomingMessage {
+  body: {
+    password: string;
+  };
+  query: string;
+  ip: string;
+  protocol: string;
+}
+
+morgan.token('body', (req: IRequest) =>
+  JSON.stringify(
+    req.body.password ? { ...req.body, password: '******' } : req.body
+  )
+);
+
+morgan.token('query', (req: IRequest) => JSON.stringify(req.query));
+
+morgan.token('ip', (req: IRequest) => JSON.stringify(req.ip));
+
+morgan.token('protocol', (req: IRequest) => JSON.stringify(req.protocol));
+
+morgan.token('host', req => req.headers.host);
+
+const options = {
+  file: {
+    json: true,
+    datePattern: 'DD-MM-YYYY',
+    handleExceptions: false,
+    handleRejections: false,
+    zippedArchive: true,
+    maxSize: '5m',
+    maxFiles: '7d'
+  },
+  console: {
+    level: 'debug',
+    handleExceptions: false,
+    handleRejections: false,
+    json: false,
+    colorize: true
+  }
+};
+
+const infoTransport = new winston.transports.DailyRotateFile({
+  ...options.file,
+  level: 'info',
+  filename: `${__dirname}/../logs/rest-service-info-%DATE%.log`
+});
+
+const errorTransport = new winston.transports.DailyRotateFile({
+  ...options.file,
+  level: 'error',
+  filename: `${__dirname}/../logs/rest-service-ERRORS-%DATE%.log`
+});
+
+const timestampFormat = () =>
+  new Date().toLocaleString('ru-RU', {
+    timeZone: 'Asia/Omsk',
+    hour12: false
+  });
+
+const logger = winston.createLogger({
+  transports: [infoTransport, errorTransport],
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.combine(),
+    winston.format.timestamp({
+      format: timestampFormat
+    }),
+    winston.format.printf(
+      info => `[${info['timestamp']} UTC+6] ${info.level}: ${info.message}`
+    )
+  ),
+  exitOnError: true
+});
+
+if (NODE_ENV === 'development') {
+  logger.add(new winston.transports.Console(options.console));
+}
+
+const stream: StreamOptions = {
+  write: (message) => {
+    logger.info(message);
+  },
+};
+
+export { stream as logger };
