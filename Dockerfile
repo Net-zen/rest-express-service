0fe9f719e44985cda2a66dd39ebb58ab5c7c6e46
Net-zen <prankster@myttk.ru>
2021-06-18 00:56:30 +0600

    feat: add connection to database

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,4 @@ WORKDIR /usr/src/app
 COPY package*.json ./
 RUN npm install
 COPY . .
-EXPOSE ${PORT}
 CMD [ "npm", "run", "dev" ]
---
+++ src/ormconfig.ts
@@ -0,0 +1,24 @@
+import {ConnectionOptions} from 'typeorm';
+import path from 'path';
+import { POSTGRES_PORT, POSTGRES_USER, POSTGRES_PASSWORD,
+  POSTGRES_DB, POSTGRES_HOST } from './common/config';
+
+
+export const config: ConnectionOptions = {
+  type: 'postgres',
+  host: POSTGRES_HOST || 'localhost',
+  port: POSTGRES_PORT ? parseInt(POSTGRES_PORT, 10) : 5432,
+  username: POSTGRES_USER || 'postgres',
+  password: POSTGRES_PASSWORD || 'password',
+  database: POSTGRES_DB || 'postgres',
+  entities: [path.join(__dirname, '/**/*.model{.ts,.js}')],
+  synchronize: false,
+  migrationsRun: true,
+  connectTimeoutMS: 60000,
+  logging: true,
+  logger: 'file',
+  migrations: [path.join(__dirname, '/database/migrations/**/*{.ts,.js}')],
+  cli: {
+    migrationsDir: 'src/migrations',
+  },
+};
