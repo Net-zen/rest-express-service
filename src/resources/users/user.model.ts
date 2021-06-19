b98082607c031f80f444ead674f240137ce8142c
Net-zen <prankster@myttk.ru>
2021-06-19 17:34:39 +0600

    feat: implement work with real database for users

--- src/resources/users/user.model.ts
+++ src/resources/users/user.model.ts
@@ -1,34 +1,58 @@
-import { v4 as uuid } from 'uuid';
-import { IUser } from '../../common/types';
+import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
 
-class User implements IUser{
+@Entity()
+export class User {
 
-  id:string;
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
 
-  name:string;
+  @Column()
+  name!: string;
 
-  login:string;
+  @Column()
+  login!: string;
 
-  password:string
-
-
-  constructor({
-    id = uuid(),
-    name = 'USER',
-    login = 'user',
-    password = 'P@55w0rd'
-  } = {}) {
-    this.id = id;
-    this.name = name;
-    this.login = login;
-    this.password = password;
-  }
-
-  static toResponse(user:IUser):IUser {
-    const { id, name, login } = user;
-    return { id, name, login };
-  }
+  @Column()
+  password!: boolean;
 
 }
 
-export default User;
+export const toResponse = (user:User):Omit<User, 'password'> => {
+  const { id, name, login } = user;
+  return { id, name, login };
+};
+
+// import { v4 as uuid } from 'uuid';
+// import { IUser } from '../../common/types';
+//
+// class User implements IUser{
+//
+//   id:string;
+//
+//   name:string;
+//
+//   login:string;
+//
+//   password:string
+//
+//
+//   constructor({
+//     id = uuid(),
+//     name = 'USER',
+//     login = 'user',
+//     password = 'P@55w0rd'
+//   } = {}) {
+//     this.id = id;
+//     this.name = name;
+//     this.login = login;
+//     this.password = password;
+//   }
+//
+//   static toResponse(user:IUser):IUser {
+//     const { id, name, login } = user;
+//     return { id, name, login };
+//   }
+//
+// }
+//
+// export default User;
--- src/resources/users/user.router.ts
+++ src/resources/users/user.router.ts
@@ -1,29 +1,28 @@
 import express from 'express';
 import { ReasonPhrases, StatusCodes } from 'http-status-codes';
-import User from './user.model';
+import { toResponse } from './user.model';
 import usersService from './user.service';
 
 const router = express.Router();
 
 router.route('/').get(async (_req, res) => {
     const users = await usersService.getAll();
-    res.json(users.map(User.toResponse));
+    res.json(users.map(toResponse));
 });
 
 router.route('/:id').get(async (req, res) => {
     const user = await usersService.get(req.params.id);
-    return res.json(User.toResponse(user));
+    return res.json(toResponse(user));
 });
 
 router.route('/').post(async (req, res) => {
-    const user = await usersService.create(
-      new User({ ...req.body }));
-    return res.status(StatusCodes.CREATED).json(User.toResponse(user));
+    const user = await usersService.create({ ...req.body });
+    return res.status(StatusCodes.CREATED).json(toResponse(user));
 });
 
 router.route('/:id').put( async (req, res) => {
     const user = await usersService.update(req.params.id, { ...req.body });
-    return res.json(User.toResponse(user));
+    return res.json(toResponse(user));
 });
 
 router.route('/:id').delete(async (req, res) => {
