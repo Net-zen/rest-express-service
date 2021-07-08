a3150ca098c52e422b04b7ee2b6553e95d97a0f7
Net-zen <prankster@myttk.ru>
2021-07-08 14:15:23 +0600

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import * as yaml from 'js-yaml';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 
 async function bootstrap() {
   const PORT = parseInt(process.env.PORT) || 4000;
   const app = await NestFactory.create(AppModule);
+  const swaggerDocument = yaml.load(
+    readFileSync(join(__dirname, '../doc/api.yaml'), 'utf-8'),
+  ) as OpenAPIObject;
+  SwaggerModule.setup('/doc', app, swaggerDocument);
   await app.listen(PORT);
 }
 bootstrap();
