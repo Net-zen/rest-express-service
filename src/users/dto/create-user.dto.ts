c73efa83c0f9df565c0e05cdf7ab3778c3d6605f
Net-zen <prankster@myttk.ru>
2021-07-27 22:15:25 +0600

    fix: remove columns from dtos

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,19 +1,15 @@
-import { Column } from 'typeorm';
 import { IsString, Length } from 'class-validator';
 
 export class CreateUserDto {
   @IsString({ message: 'Have to be string' })
-  @Column()
   name: string;
 
   @IsString({ message: 'Have to be string' })
-  @Column()
   login: string;
 
   @IsString({ message: 'Have to be string' })
   @Length(5, 25, {
     message: 'Net less then 5 symbols, and not more then 25 symbols',
   })
-  @Column()
   password: string;
 }
